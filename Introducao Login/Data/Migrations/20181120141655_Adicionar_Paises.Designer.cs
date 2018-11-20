33e95d6093f5d6c5e80a773ae490b7595885d36a
Anderson Jardim <andersonjardim@gmail.com>
2018-11-20 12:21:11 -0200

    COMANDO NO CONSOLE: Add-Migration Adicionar_Paises update-database

--- Introducao Login/Data/Migrations/20181120141655_Adicionar_Paises.Designer.cs
+++ Introducao Login/Data/Migrations/20181120141655_Adicionar_Paises.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Introducao_Login.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181120141655_Adicionar_Paises")]
+    partial class Adicionar_Paises
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
